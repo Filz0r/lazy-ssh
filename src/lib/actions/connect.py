628692af054bcab034f6f5950daae429e6697f7d
Filipe Figueiredo <70453488+Filz0r@users.noreply.github.com>
2021-05-05 15:10:43 +0100

    cleaned up the final command

--- src/lib/actions/connect.py
+++ src/lib/actions/connect.py
@@ -8,12 +8,13 @@ from subprocess import run
 # are used to handle options that are passed to lzh, lzh then builds a string that
 # consists on the command that you would usually pass to ssh according to the options
 # that are passed to lzh
+
+
 def make_connection(host_info,
                     internal_options=None,
                     external_options=None,
                     port=22,
                     command=None):
-    print(command)
     if command != None:
         command_targets = command['targets']
         command_options = command['options']
@@ -23,19 +24,19 @@ def make_connection(host_info,
         if '-u' in command_options:
             user_index = command_options.index('-u')
             host_info['user'] = command_targets[user_index]
-        
+
     if host_info['key_path'] != None and port == 22:
-        final_comand = f"ssh -i {host_info['key_path']} {host_info['user']}@{host_info['ip']}"
+        final_comand = f"ssh {host_info['user']}@{host_info['ip']} -i {host_info['key_path']} "
     elif host_info['key_path'] != None and port != 22:
         final_comand = f"ssh -i {host_info['key_path']} {host_info['user']}@{host_info['ip']} -p {port}"
     else:
         final_comand = f"ssh {host_info['user']}@{host_info['ip']}"
-        
+
     if '-d' in command_options:
         final_comand = final_comand + ' -v'
     elif '-ddd' in command_options:
         final_comand = final_comand + ' -vvv'
     # keep this here for debugging reasons, it's easier to find out what's breaking the program this way
-    #print(final_comand)
+    # print(final_comand)
     splited_cmd = final_comand.split()
     return run(splited_cmd)
